c3a3d7323d9a1bf2e78373d15ec87051298c1502
Surafele Asfaw <surafeleasfaw@Surafeles-MacBook-Air.local>
2022-11-08 16:21:16 -0800

    constant array and 4digit prompt

---
+++ js/javascript.js
@@ -0,0 +1,33 @@
+const courseList = [
+    { 
+        code: "1620",
+        class: "Web Fundamentals Technologies"
+    },
+    {
+        code: "1420",
+        class: "System Administration"
+    },
+    {
+        code: "1630",
+        class: "Database Systems"
+    },
+];
+
+let code = prompt("Enter code to find class:");
+
+
+while (code.length != 4 || (isNaN(Number(code))))
+console.log(code = prompt("Invalid input:"))
+
+
+
+
+
+
+// do {
+//     code = prompt("Enter code to find class:");
+// } while (user_input.length = 4 && !isNaN(user_input))
+//     if (user_input = "1620" || "1420" || "1630")
+
+    
+
